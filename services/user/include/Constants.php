7f4c31228f3dc377d482ce485cc47f1dbb0c264b
Dev <devjariwala145@gmail.com>
2024-06-28 20:11:17 +0530

    removed service folder

--- services/user/include/Constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Constants to connect with the database
-define('DB_USERNAME', 'pragmanx_kapot');
-define('DB_PASSWORD', 'b}inIygGHQTS');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'pragmanx_kapotmail');
